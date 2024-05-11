f7e8ff822bbf4a49f463a49ccdf62e82a15739b3
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-05-11 08:14:23 -0700

    put route added

--- index.js
+++ index.js
@@ -55,9 +55,19 @@ async function run() {
       const donation = req.body;
       const query = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set: {},
+        $set: {
+          image: donation.image,
+          title: donation.title,
+          category: donation.category,
+          amount: donation.amount,
+        },
       };
-      const result = await donationsCollection.updateOne(query, updateDoc);
+      const options = { upsert: true };
+      const result = await donationsCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
 
       res.send(result);
     });
@@ -68,7 +78,7 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
+      "Pinged your deploymentyyy. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
